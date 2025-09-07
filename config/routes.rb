095fcf0afbe022f99619f082f655644c9b277651
matsu-sy <k.c.e.38m@gmail.com>
2025-09-07 17:08:14 +0900

    トップとモーダル設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  root "home#index"
+  # アプリケーションのルート
+  root "top#top"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -9,7 +10,7 @@ Rails.application.routes.draw do
   # Render dynamic PWA files from app/views/pwa/*
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+  
+  # Chrome開発者ツール用（任意）
+  get '/.well-known/appspecific/com.chrome.devtools.json', to: proc { [404, {}, ['']] }
 end
